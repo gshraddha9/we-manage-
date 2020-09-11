d82825338cc3caca55560f80d95b87643ed8d2da
gshraddha9 <66221716+gshraddha9@users.noreply.github.com>
2020-09-11 19:11:12 +0530

    Create .eslintrc.js

---
+++ src/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    },
+    "extends": "eslint:recommended"
+    ,
+   "extends": "airbnb-base" 
+};
